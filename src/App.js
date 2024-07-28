e59d6575d31c4f7c5ff5da6b9ba57f83f04d001f
Chana Verzberger <chanav@realcommerce.co.il>
2024-07-28 12:47:42 +0300

    fix route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Home from './components/Home';
 import ItemDetails from './components/ItemDetails';
-import { Routes, Route } from 'react-router-dom';
-import './App.css'
-
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
 function App() {
 
     return (
         <div className="App">
-            <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/item/:id" element={<ItemDetails/>} />
-            </Routes>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/item/:id" element={<ItemDetails/>} />
+                </Routes>
+            </Router>
         </div>
     );
 }
